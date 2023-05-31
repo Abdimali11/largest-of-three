4369c35923d7d9877d33c68f959dae0c5b4672cf
Abdimalik11 <ilkacaze5522@gmail.com>
2023-05-30 17:22:32 -0700

    this is the Answer of largest-of-three v2

--- challenge.js
+++ challenge.js
@@ -1,2 +1,16 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let a=2
+let b=4
+let c=6
+if (a>b&&a>c){
+    console.log("The largest number out of the three numbers is " + a)
+}else if(b>a&&b>c){
+    console.log("The largest number out of the three numbers is " + b)
+
+}else if (c>a&&c>b){
+    console.log("The largest number out of the three numbers is " + c)
+}
+else{
+    console.log("there is no a number")
+}
